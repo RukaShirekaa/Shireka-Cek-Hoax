6b0e48a215e616a1d331fe7aaea7df00c1efc331
Shirekaa <khanako872@gmail.com>
2026-05-30 12:57:29 +0800

    api

--- shireka-fakta/app.py
+++ shireka-fakta/app.py
@@ -9,11 +9,9 @@ import google.generativeai as genai  # ✅ Correct
 from dotenv import load_dotenv
 
 load_dotenv()
-api_key = os.getenv('GEMINI_API_KEY')
-genai.configure(api_key=api_key)
 API_KEY = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 app = Flask(__name__)
 
 
